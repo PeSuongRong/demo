6528a84520cdcfcfbfc05594ed8fde9f1708029e
DESKTOP-UHS7DTJ\AGL <fiststar1995@gmail.com>
2020-09-29 14:53:54 +0700

    add

--- src/components/secHome2.vue
+++ src/components/secHome2.vue
@@ -75,17 +75,17 @@ var listSecHome2 = [
     {
         title:"結婚に",
         info: "結婚式をもっと自由に。もっと楽しく。<br>どこでも、何度でも！<br>素敵な結婚式を増やします。",
-        img: "./img/img-top1-small1.702c31de.svg"
+        img: "img/img-top1-small1.702c31de.svg"
     },
     {
         title:"住まいに",
         info: "住まいの売り買いって、難しそう。<br>もっと気軽に、もっと賢くできるはず。<br>住まいに対する疑問やストレスを減らします。",
-        img: "./img/img-top1-small2.2d0a30ec.svg"
+        img: "img/img-top1-small2.2d0a30ec.svg"
     },
     {
         title:"お金に",
         info: "お金って身近だけど、よくわからない。<br>しかし、とても重要。<br>お金に関して「学ぶ」、「問題を解決する」ことを応援します。",
-        img: "./img/img-top1-small3.f72827f6.svg"
+        img: "img/img-top1-small3.f72827f6.svg"
     },
 ]
 export default {
