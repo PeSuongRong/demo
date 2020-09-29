6af22a29aefe74a106b7378e2192ba0e63426706
DESKTOP-UHS7DTJ\AGL <fiststar1995@gmail.com>
2020-09-29 09:56:02 +0700

    add

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,24 @@
 <template>
   <div id="app">
-    <CompHeader></CompHeader>
-    <router-link to="/contact">Contact</router-link>
+    <compHeader></compHeader>
     <router-view />
-    <CompFooter></CompFooter>
+    <compFooter></compFooter>
   </div>
 </template>
 
 <script>
-import $ from 'jquery'
-import CompHeader from './components/compHeader'
-import CompFooter from './components/compFooter'
+import compHeader from './components/compHeader.vue'
+import compFooter from './components/compFooter.vue'
 
 export default {
-  name: 'app',
-  data () {
-    return {
-      
-    }
-  },
+  name: 'App',
   components: {
-    CompHeader,
-    CompFooter
-
-  },
-  mounted(){
-    $(document).ready(function(){
-    $(".c-menu--list >ul>li").click(function(){
-        $(this).find('ul').toggleClass('is-active');
-        $(this).find('p span').toggleClass('is-show');
-    });
-    $(".c-menu-ft>li").click(function(){
-        $(this).find('.c-menu-ft--child').toggleClass('is-active');
-    });
-    $(".c-menu--close").click(function(){
-        $(".c-menu--list").addClass('is-hide');
-        $(this).hide();
-    });
-    $(".c-menu--open").click(function(){
-        $(".c-menu--list").removeClass('is-hide');
-        $(".c-menu--close").show();
-    });
-    $(".l-presroom1--btn .c-btn2").click(function(){
-        $(this).hide();
-        $(".l-presroom1--info .l-news+.l-news").show();
-        $(".l-presroom1--info .l-news:first-child").hide();
-    });
-    $('.jss1').on('click', function() {
-        $('.jss1').removeClass('is-act');
-        $(this).addClass('is-act');
-    });
-    $('html').click(function(event) {
-        if ($(event.target).closest('.jss1').length === 0) {
-            $('.jss1').removeClass('is-act');
-        }
-    });
-  });
-}}
+    compHeader,
+    compFooter
+  }
+}
 </script>
 
 <style lang="scss">
-  @import './css/all.scss'
+  @import 'scss/all.scss';
 </style>
