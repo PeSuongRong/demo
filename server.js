0574409f760b259bd0a2466e9ee73639069b1247
DESKTOP-UHS7DTJ\AGL <fiststar1995@gmail.com>
2020-10-01 13:15:20 +0700

    add product

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
+// eslint-disable-next-line no-undef
 const express = require('express')
+// eslint-disable-next-line no-undef
 const serveStatic = require('serve-static')
+// eslint-disable-next-line no-undef
 const path = require('path')
 const app = express()
 
+// eslint-disable-next-line no-undef
 app.use('/', serveStatic(path.join(__dirname,'/dist')))
 
+// eslint-disable-next-line no-undef
 const port = process.env.PORT || 8080
 
 app.listen(port, () => {
--- src/views/info/products.vue
+++ src/views/info/products.vue
@@ -15,7 +15,7 @@
           >
             <figure>
               <img
-                :src="item.img"
+                :src="require(`../../assets/products/img${index+1}.jpg`)"
                 :alt="item.title"
               >
             </figure>
@@ -54,91 +54,91 @@
 <script>
 var listProduct = [
     {
-        img: '/dist/img/products/img1.jpg',
+        img: '../../assets/products/img1.jpg',
         title: '結婚関連事業',
         info: '「えらべる結婚式をお得な価格でつくる」を事業ミッションに掲げ、メディアを通じた情報提供から結婚式当日のサービス提供まで、花嫁花婿の結婚式づくりを一気通貫でサポートする体制を強化しています。主なサービスとして、結婚式場の情報や、それに関わる口コミや実際の費用明細などをお伝えするウェディング総合情報メディア「みんなのウェディング」、会費制結婚式のプロデュースサービス「会費婚」、インポートブランドを中心としたウェディングドレスを販売する「DRESS EVERY」などを運営しています。',
         logo:[
             {
                 link: 'https://www.mwed.jp/',
-                src: '/dist/img/products/logo1.svg'
+                src: '../../../logo/logo1.svg'
             },
             {
                 link: 'https://www.kaihikon.com/',
-                src: '/dist/img/products/logo2.svg'
+                src: '../../../logo/logo2.svg'
             },
             {
                 link: 'http://dress-every.com/',
-                src: '/dist/img/products/logo3.png'
+                src: '../../../logo/logo3.png'
             }
         ]
 
     },
      {
-        img: '/dist/img/products/img2.jpg',
+        img: '../../assets/products/img2.jpg',
         title: '不動産関連事業',
         info: '住宅・不動産業界の情報格差をなくすことで、ユーザーが住まいの売買・賃貸を快適に行えるサービスの提供を目指し、住まい探しの情報提供を行うポータルサイト「オウチーノ」、不動産売買サービス「おうちのくふう」を運営しています。住宅・不動産会社向けには、住宅を探しているユーザーのニーズに合わせて提案可能な不動産営業支援ツール「くらすマッチ」も提供しています。また、Seven Signatures Internationalは富裕層のお客様向けのコンサルティングサービスも提供しています。',
         logo:[
             {
                 link: 'https://o-uccino.com/front',
-                src: '/dist/img/products/logo4.svg'
+                src: '../../../logo/logo4.svg'
             },
             {
                 link: 'https://kurasumatch.o-uccino.com/areagram/landing/new',
-                src: '/dist/img/products/logo5.svg'
+                src: '../../../logo/logo5.svg'
             },
             {
                 link: 'https://www.sevensignatures.com/',
-                src: '/dist/img/products/logo6.svg'
+                src: '../../../logo/logo6.svg'
             },
             {
                 link: 'https://ouchi-kufu.com/',
-                src: '/dist/img/products/logo7.svg'
+                src: '../../../logo/logo7.svg'
             }
         ]
     },
     {
-        img: '/dist/img/products/img3.jpg',
+        img: '../../assets/products/img3.jpg',
         title: '金融関連事業',
         info: 'お金の面から一人ひとりの暮らしに寄り添い、行動の変化に繋げることを目指して、国内最大級の家計簿サービス「Zaim」を提供しています。また、ライフスタイルの変化に応じてニーズの高まる保険商品を開発する「くふう少額短期保険」、保険商品の検討支援を行う「保険のくふう」を運営しています',
         logo:[
             {
                 link: 'https://zaim.net/',
-                src: '/dist/img/products/logo8.svg'
+                src: '../../../logo/logo8.svg'
             },
             {
                 link: 'https://hoken-kufu.com/',
-                src: '/dist/img/products/logo9.svg'
+                src: '../../../logo/logo9.svg'
             },
             {
                 link: 'https://www.2960owl.jp/',
-                src: '/dist/img/products/logo10.svg'
+                src: '../../../logo/logo10.svg'
             }
         ]
     },
     {
-        img: '/dist/img/products/img4.jpg',
+        img: '../../assets/products/img4.jpg',
         title: 'デザイン・テクノロジー関連領域',
         info: 'Da Vinci Studioを中心に、グループ内外の制作・開発業務の支援、およびそれらを通じたノウハウ蓄積、研究開発に取り組んでいます。',
         logo:[
             {
                 link: 'https://da-vinci-studio.com/',
-                src: '/dist/img/products/logo11.png'
+                src: '../../../logo/logo11.png'
             }
         ]
     },
      {
-        img: '/dist/img/products/img5.jpg',
+        img: '../../assets/products/img5.jpg',
         title: 'メディア関連領域',
         info: '膨大かつ複雑な情報に接する機会が増大する中で、ユーザーと事業者の情報格差をなくし、ユーザーのニーズを捉えた情報を分かりやすく伝える情報環境の構築を、くらしにくふう社を中心に展開しています。暮らしのトレンドマガジン「ヨムーノ」を運営しているほか、圧倒的なユーザーファーストメディアをくふうグループの各事業領域で構築・支援していきます。',
         logo:[
             {
                 link: 'https://www.o-uccino.jp/article',
-                src: '/dist/img/products/logo12.svg'
+                src: '../../../logo/logo12.svg'
             }
         ]
     },
     {
-        img: '/dist/img/products/img6.jpg',
+        img: '../../assets/products/img6.jpg',
         title: '投資・起業家支援領域',
         info: '当社グループが保有する投資資源を活用して、起業家支援、ベンチャー企業の経営支援、新規事業創出に取り組んでいます。'
     }
@@ -149,7 +149,7 @@ export default {
     return {
         list: listProduct
     }
-  }
+  },
 }
 </script>
 
