5ebc83d0838e36ffeed6df88dcce195493651ce3
DESKTOP-UHS7DTJ\AGL <fiststar1995@gmail.com>
2020-10-01 10:58:45 +0700

    test routes

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,55 @@
 import Page404 from './components/comp404'
 import Home from './views/home'
 import Security from './views/security'
+import Info from './views/info/info'
+import Access from './views/info/access'
+import Boards from './views/info/boards'
+import History from './views/info/history'
+import Philosophy from './views/info/philosophy'
+import Products from './views/info/products'
+import Profile from './views/info/profile'
 
 export const routes = [
     {
-        path: '/', name: 'Home', component: Home
+        path: '/', name: 'Home', component: Home,
+        meta: {
+            title: "Home-kufu",
+            forVisitors: true
+        }
     },
     {
-        path: '*', name: 'Page404', component: Page404
+        path: '*', name: 'Page404', component: Page404,
+        meta: {
+            title: "404",
+            forVisitors: true
+        }
     },
     {
-        path: '/security', name: 'Security', component: Security
+        path: '/security', name: 'Security', component: Security,
+        meta: {
+            title: "Security-kufu",
+            forVisitors: true
+        }
+    },
+    {
+        path: '/about/info', name: 'Info', component: Info
+    },
+    {
+        path: '/about/info/access', name: 'Access', component: Access
+    },
+    {
+        path: '/about/info/boards', name: 'Boards', component: Boards
+    },
+    {
+        path: '/about/info/history', name: 'History', component: History
+    },
+    {
+        path: '/about/info/philosophy', name: 'Philosophy', component: Philosophy
+    },
+    {
+        path: '/about/info/products', name: 'Products', component: Products
+    },
+    {
+        path: '/about/info/profile', name: 'Profile', component: Profile
     }
 ]
